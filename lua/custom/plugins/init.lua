9344eab26022e36a86329f23ab3e317d6cd67838
sbelmore <shawnbelmore@MacBookPro.attlocal.net>
2024-12-09 18:14:42 -0600

    QOL changes
    Added command-line exiting to 'jk', simplified git commands, simplified
    Leap keymaps, removed cinnamon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,5 @@
 -- Insert Mode Remaps:
-vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
+vim.keymap.set({ 'i', 'c' }, 'jk', '<Esc>', { desc = 'Exit insert/cmd-line mode' })
 vim.keymap.set('i', '<C-b>', '<ESC>^i', { desc = 'Beginning of line' })
 -- Normal Mode Remaps:
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Page Down Centered' })
@@ -8,39 +8,34 @@ vim.keymap.set('n', 'H', 'Hzz', { desc = 'Middle Page Centered' })
 vim.keymap.set('n', 'G', 'Gzz', { desc = 'EOF Centered' })
 vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join Lines' })
 -- vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', { desc = 'New Buffer' }) --Don't really find myself using this
-vim.keymap.set('n', '<leader><tab> ', '<cmd> bn <CR>', { desc = 'Next Buffer' })
-vim.keymap.set('n', '<leader>,', '<cmd> bp <CR>', { desc = 'Prev Buffer' })
-vim.keymap.set('n', '<leader>g%', '<cmd> Git commit % <CR>', { desc = 'Commit this file' })
-vim.keymap.set('n', '<leader>g.', '<cmd> Git commit .<CR>', { desc = 'Commit all files' })
-vim.keymap.set('n', '<leader>gp', '<cmd> Git push <CR>', { desc = 'Git push' })
+vim.keymap.set('n', '<leader><tab>', '<cmd> bn <CR>', { desc = 'Next Buffer' })
+vim.keymap.set('n', '<leader><S-tab>', '<cmd> bp <CR>', { desc = 'Prev Buffer' })
+vim.keymap.set('n', '<leader>x', '<cmd> bn <CR> <cmd> bd# <CR>', { desc = 'Delete Buffer' })
+vim.keymap.set('n', '<leader>gt', '<cmd> Git commit % <CR>', { desc = 'Commit [T]his file' })
+vim.keymap.set('n', '<leader>ga', '<cmd> Git commit .<CR>', { desc = 'Commit [A]ll files' })
+vim.keymap.set('n', '<leader>gp', '<cmd> Git push <CR>', { desc = 'Git [P]ush' })
 -- Visual Mode Remaps:
 -- These are now handled by mini.move:
 -- vim.keymap.set('v', '<leader>j', " :m '>+ 1 <CR>gv=gv", { desc = 'Move Up Through Lines' })
 -- vim.keymap.set('v', '<leader>k', ':m-2<CR>gv=gv', { desc = 'Move Down Through Lines' })
-vim.keymap.set({ 'n', 'x', 'o' }, 'ls', '<Plug>(leap-forward)')
-vim.keymap.set({ 'n', 'x', 'o' }, 'lS', '<Plug>(leap-backward)')
-vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
+vim.keymap.set({ 'n', 'x', 'o' }, '<leader>l', '<Plug>(leap-forward)', { desc = 'Leap Forward' })
+vim.keymap.set({ 'n', 'x', 'o' }, '<leader>L', '<Plug>(leap-backward)', { desc = 'Leap Backward' })
+vim.keymap.set({ 'n', 'x', 'o' }, '<leader>lw', '<Plug>(leap-from-window)', { desc = 'Leap from Window' })
+
+local lspconfig = require 'lspconfig'
+lspconfig.gopls.setup {}
+
+local format_sync_grp = vim.api.nvim_create_augroup('goimports', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    require('go.format').goimports()
+  end,
+  group = format_sync_grp,
+})
 
 -- Plugins:
 return {
-  -- {
-  --   'declancm/cinnamon.nvim',
-  --   version = '*', -- use latest release
-  --   opts = {
-  --     -- change default options here
-  --   },
-  --   config = function()
-  --     require('cinnamon').setup {
-  --       -- Enable all provided keymaps
-  --       keymaps = {
-  --         basic = true,
-  --         extra = true,
-  --       },
-  --       -- Only scroll the window
-  --       options = { mode = 'window' },
-  --     }
-  --   end,
-  -- },
   {
     'ggandor/leap.nvim',
   },
@@ -70,7 +65,7 @@ return {
       {
         '<leader>A',
         function()
-          require('harpoon'):list():append()
+          require('harpoon'):list():add()
         end,
         desc = 'harpoon file',
       },
