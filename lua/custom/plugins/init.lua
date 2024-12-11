68a24de0eb6bbc77992d58a3a4974046b2bdf9a3
sbelmore <shawnbelmore@MacBookPro.attlocal.net>
2024-12-11 13:15:28 -0600

    more QOL chages
    Removed leap, added folke/flash with custom mapping to avoid conflicts
    with mini.surround, disabled formatting from gopls (formatting was
    corrupting code by removing leading characters after whitespace),
    cleaned up harpoon and added telescope integration for UI, added neoclip
    for register UI, added visual mode to 'jk' escape sequence

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,44 +1,69 @@
 -- Insert Mode Remaps:
-vim.keymap.set({ 'i', 'c' }, 'jk', '<Esc>', { desc = 'Exit insert/cmd-line mode' })
+vim.keymap.set({ 'i', 'c', 'v' }, 'jk', '<Esc>', { desc = 'Exit insert/cmd-line/visual mode' })
 vim.keymap.set('i', '<C-b>', '<ESC>^i', { desc = 'Beginning of line' })
+vim.keymap.set('i', '<C-e>', '<ESC>$a', { desc = 'End of line' })
 -- Normal Mode Remaps:
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Page Down Centered' })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Page Up Centered' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Page [D]own Centered' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Page [U]p Centered' })
 vim.keymap.set('n', 'H', 'Hzz', { desc = 'Middle Page Centered' })
 vim.keymap.set('n', 'G', 'Gzz', { desc = 'EOF Centered' })
 vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join Lines' })
 -- vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', { desc = 'New Buffer' }) --Don't really find myself using this
-vim.keymap.set('n', '<leader><tab>', '<cmd> bn <CR>', { desc = 'Next Buffer' })
-vim.keymap.set('n', '<leader><S-tab>', '<cmd> bp <CR>', { desc = 'Prev Buffer' })
-vim.keymap.set('n', '<leader>x', '<cmd> bn <CR> <cmd> bd# <CR>', { desc = 'Delete Buffer' })
+vim.keymap.set('n', '<leader><tab>', '<cmd> bn <CR>', { desc = '[N]ext Buffer' })
+vim.keymap.set('n', '<leader><S-tab>', '<cmd> bp <CR>', { desc = '[P]rev Buffer' })
+vim.keymap.set('n', '<leader>x', '<cmd> bn <CR> <cmd> bd# <CR>', { desc = 'Delete/E[x]it Buffer' })
 vim.keymap.set('n', '<leader>gt', '<cmd> Git commit % <CR>', { desc = 'Commit [T]his file' })
 vim.keymap.set('n', '<leader>ga', '<cmd> Git commit .<CR>', { desc = 'Commit [A]ll files' })
 vim.keymap.set('n', '<leader>gp', '<cmd> Git push <CR>', { desc = 'Git [P]ush' })
+vim.keymap.set('n', '<leader>gi', '<cmd> GoImport <CR>', { desc = '[G]o [I]mport' })
+vim.keymap.set('n', '""', '<cmd> Telescope neoclip <CR>', { desc = 'Telescope Registers' })
 -- Visual Mode Remaps:
--- These are now handled by mini.move:
--- vim.keymap.set('v', '<leader>j', " :m '>+ 1 <CR>gv=gv", { desc = 'Move Up Through Lines' })
--- vim.keymap.set('v', '<leader>k', ':m-2<CR>gv=gv', { desc = 'Move Down Through Lines' })
 vim.keymap.set({ 'n', 'x', 'o' }, '<leader>l', '<Plug>(leap-forward)', { desc = 'Leap Forward' })
 vim.keymap.set({ 'n', 'x', 'o' }, '<leader>L', '<Plug>(leap-backward)', { desc = 'Leap Backward' })
 vim.keymap.set({ 'n', 'x', 'o' }, '<leader>lw', '<Plug>(leap-from-window)', { desc = 'Leap from Window' })
 
-local lspconfig = require 'lspconfig'
-lspconfig.gopls.setup {}
-
-local format_sync_grp = vim.api.nvim_create_augroup('goimports', {})
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.go',
-  callback = function()
-    require('go.format').goimports()
-  end,
-  group = format_sync_grp,
-})
-
+-- require('lspconfig').gopls.setup {}
+--
+-- local format_sync_grp = vim.api.nvim_create_augroup('goimports', {})
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--   pattern = '*.go',
+--   callback = function()
+--     require('go.format').goimports()
+--   end,
+-- group = format_sync_grp,
+-- })
+--
 -- Plugins:
 return {
   {
-    'ggandor/leap.nvim',
+    'AckslD/nvim-neoclip.lua',
+    dependencies = {
+      -- you'll need at least one of these
+      { 'nvim-telescope/telescope.nvim' },
+      -- {'ibhagwan/fzf-lua'},
+    },
+    config = function()
+      require('neoclip').setup()
+    end,
   },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@diagnostic disable-next-line
+    ---@type Flash.Config
+    opts = {},
+  -- stylua: ignore
+  keys = {
+    { "fs", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "fS", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "fr", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "fR", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+  },
+  -- {
+  --   'ggandor/leap.nvim',
+  -- },
   {
     'ray-x/go.nvim',
     dependencies = { -- optional packages
@@ -55,77 +80,67 @@ return {
   },
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   {
-    'theprimeagen/harpoon',
+    'ThePrimeagen/harpoon',
     branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('harpoon'):setup()
-    end,
-    keys = {
-      {
-        '<leader>A',
-        function()
-          require('harpoon'):list():add()
-        end,
-        desc = 'harpoon file',
-      },
-      {
-        '<leader>a',
-        function()
-          local harpoon = require 'harpoon'
-          harpoon.ui:toggle_quick_menu(harpoon:list())
-        end,
-        desc = 'harpoon quick menu',
-      },
-      {
-        '<leader>1',
-        function()
-          require('harpoon'):list():select(1)
-        end,
-        desc = 'harpoon to file 1',
-      },
-      {
-        '<leader>2',
-        function()
-          require('harpoon'):list():select(2)
-        end,
-        desc = 'harpoon to file 2',
-      },
-      {
-        '<leader>3',
-        function()
-          require('harpoon'):list():select(3)
-        end,
-        desc = 'harpoon to file 3',
-      },
-      {
-        '<leader>4',
-        function()
-          require('harpoon'):list():select(4)
-        end,
-        desc = 'harpoon to file 4',
-      },
-      {
-        '<leader>5',
-        function()
-          require('harpoon'):list():select(5)
-        end,
-        desc = 'harpoon to file 5',
-      },
-      {
-        '<leader><tab>',
-        function()
-          require('harpoon'):list():next()
-        end,
-        desc = 'next stored buffer',
-      },
-      {
-        '<leader>,',
-        function()
-          require('harpoon'):list():prev()
-        end,
-        desc = 'next stored buffer',
-      },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
     },
+    config = function()
+      config = function()
+        require('harpoon').setup()
+      end
+
+      local conf = require('telescope.config').values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require('telescope.pickers')
+          .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+              results = file_paths,
+            },
+            previewer = conf.file_previewer {},
+            sorter = conf.generic_sorter {},
+          })
+          :find()
+      end
+
+      vim.keymap.set('n', '<C-e>', function()
+        require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())
+      end, { desc = 'Harpoon Files Quick Menu' })
+      vim.keymap.set('n', '<leader>A', function()
+        require('harpoon'):list():add()
+      end, { desc = '[A]dd Harpoon File' })
+      vim.keymap.set('n', '<leader>a', function()
+        toggle_telescope(require('harpoon'):list())
+      end, { desc = 'Telescope Harpoon Files' })
+
+      vim.keymap.set('n', '<leader>1', function()
+        require('harpoon'):list():select(1)
+      end, { desc = 'Harpoon File [ 1 ]' })
+      vim.keymap.set('n', '<leader>2', function()
+        require('harpoon'):list():select(2)
+      end, { desc = 'Harpoon File [ 2 ]' })
+      vim.keymap.set('n', '<leader>3', function()
+        require('harpoon'):list():select(3)
+      end, { desc = 'Harpoon File [ 3 ]' })
+      vim.keymap.set('n', '<leader>4', function()
+        require('harpoon'):list():select(4)
+      end, { desc = 'Harpoon File [ 4 ]' })
+      vim.keymap.set('n', '<leader>5', function()
+        require('harpoon'):list():select(5)
+      end, { desc = 'Harpoon File [ 5 ]' })
+
+      vim.keymap.set('n', '<leader>p', function()
+        harpoon:list():prev()
+      end, { desc = 'Harpoon [P]revious Buffer' })
+      vim.keymap.set('n', '<leader>n', function()
+        harpoon:list():next()
+      end, { desc = 'Harpoon [N]ext Buffer' })
+    end,
   },
 }
